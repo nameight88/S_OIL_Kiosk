eccc5e1f5ae68116496db6cbd197ef57cb571993
nam david <161475976+nameight88@users.noreply.github.com>
2025-12-04 00:03:05 +0900

    Delete SMT_Manager directory

--- SMT_Manager/src/SMT_Manager/ImageProto.h
+++
@@ -1,7 +0,0 @@
-
-#define IMG_KIOSK              	_T("..\\skin\\kiosk.png")
-#define IMG_LOCKER_SEL_S_BBOX	_T("..\\skin\\B_box.png")
-#define IMG_LOCKER_SEL_S_GBOX	_T("..\\skin\\G_box.png")
-#define IMG_LOCKER_SEL_S_RBOX	_T("..\\skin\\R_box.png")
-#define IMG_LOCKER_BG	        _T("..\\skin\\locker_bg.png")
-#define IMG_LOCKER_TITLE    	_T("..\\skin\\lockertitlesmall.png")
